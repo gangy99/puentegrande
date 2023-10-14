98fe78d7adca0a83cfe9726f8c439f2df242bcb6
Angy <diseno5@Arte-y-Diseno.local>
2023-10-13 18:17:43 -0600

    Agregado de codifo form

--- js/mail/form.js
+++ js/mail/form.js
@@ -12,7 +12,8 @@ function handleSendEmail(event) {
         message: formData.get("message")
     };
 
-    const emailBody = `Nombre: ${data.name}%0D%0A
+    const emailBody = 
+    `Nombre: ${data.name}%0D%0A
     Email: ${data.email}%0D%0A
     Teléfono: ${data.phone}%0D%0A
     Mensaje: ${data.message}`;
@@ -48,6 +49,32 @@ form.addEventListener("submit", handleSendEmail)
 //             body: JSON.stringify(data)
 //         };
 //         fetch("https://example.com/sendmail.php", requestOptions)
+//             .then(response => response.json())
+//             .then(data => console.log(data))
+//             .catch(error => console.error("Error:", error));
+//     });
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const form = document.querySelector("form");
+//     form.addEventListener("submit", function(event) {
+//         event.preventDefault();
+//         const formData = new FormData(form);
+//         const data = {
+//             name: formData.get("name"),
+//             email: formData.get("email"),
+//             phone: formData.get("phone"),
+//             message: formData.get("message")
+//         };
+//         const requestOptions = {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify(data)
+//         };
+//         fetch("https://smtp.elasticemail.com", requestOptions)
 //             .then(response => response.json())
 //             .then(data => console.log(data))
 //             .catch(error => console.error("Error:", error));
